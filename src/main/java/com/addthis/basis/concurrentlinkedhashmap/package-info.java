98256dcc2d6f49fda65018fba8f209292fdd9e72
Michael Spiegel <spiegel@addthis.com>
2013-12-18 16:38:27 -0500

    Initial commit of the MediatedEvictionConcurrentHashMap, derivated from Ben Manes ConcurrentLinkedHashMap but allows for a mediating interface to select which keys are eviction and possibly perform a side-effect operation prior to eviction.

---
+++ src/main/java/com/addthis/basis/concurrentlinkedhashmap/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Derived from https://code.google.com/p/concurrentlinkedhashmap/
+ * Under the Apache License 2.0 http://www.apache.org/licenses/
+ * https://code.google.com/p/concurrentlinkedhashmap/source/browse/LICENSE
+ */
+package com.addthis.basis.concurrentlinkedhashmap;
